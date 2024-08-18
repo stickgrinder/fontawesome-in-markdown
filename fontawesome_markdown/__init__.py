b5785577f9d9349800798850aa69e00493fad582
Paolo Pustorino <stickgrinder@gmail.com>
2024-08-18 12:29:54 +0200

    Module updated for python-markdown > 3.4.

--- fontawesome_markdown/__init__.py
+++ fontawesome_markdown/__init__.py
@@ -1,1 +1,1 @@
-from .main import FontAwesomePattern, FontAwesomeExtension, FontAwesomeException, makeExtension  # NOQA
+from .main import FontAwesomeInlineProcessor, FontAwesomeExtension, FontAwesomeException, makeExtension # NOQA
--- fontawesome_markdown/main.py
+++ fontawesome_markdown/main.py
@@ -1,15 +1,16 @@
 from __future__ import unicode_literals
 from markdown.extensions import Extension
-from markdown.inlinepatterns import Pattern
-from markdown.util import etree
+from markdown.inlinepatterns import InlineProcessor
+import xml.etree.ElementTree as etree
 from .icon_list import icons
 import json
+import re
 
 fontawesome_pattern = r':(fa[bsrl]?)?\s?fa-([-\w]+)\s?(fa-(xs|sm|lg|[\d+]x|10x))?:'
 
 prefix_to_style = {
-    'fas': 'solid',
     'fa': 'solid',
+    'fas': 'solid',
     'fab': 'brands',
     'far': 'regular',
     'fal': 'light',
@@ -23,47 +24,46 @@ class FontAwesomeException(Exception):
     pass
 
 
-class FontAwesomePattern(Pattern):
-    'Markdown pattern class for matching things that look like FA icons'
+class FontAwesomeInlineProcessor(InlineProcessor):
+    'Markdown inline processor class for matching things that look like FA icons'
 
-    def handleMatch(self, m):
+    def handleMatch(self, m, data):
         el = etree.Element('i')
-        prefix = m.group(2)
-        icon_name = m.group(3)
-        size = m.group(4)
+        prefix = m.group(1)
+        icon_name = m.group(2)
+        size = m.group(3)
         if icon_name in icons:
             styles = icons[icon_name]
+            # if the prefix is not specified, default to 'fa' for solid icons
             if not prefix and 'solid' in styles:
-                # default prefix is fa
                 prefix = 'fa'
+            # if the prefix is not specified and the icon has no solid style,
+            # default to the first available style
             elif not prefix and 'solid' not in styles:
                 style = styles[0]
                 prefix = style_to_prefix.get(style)
                 if not prefix:
-                    message = "unknown style {0}".format(style)
-                    raise FontAwesomeException(message)
-
+                    # if the first style is not supported, raise an exception
+                    raise FontAwesomeException(f"Unknown guessed style {style} for icon {icon_name}")
+            # if the specified prefix is not supported, raise an exception
+            elif prefix and prefix not in prefix_to_style:
+                raise FontAwesomeException(f"Unsupported prefix {prefix}.\nAllowed prefixes are {prefix_to_style.keys()}")
+            # if the specified prefix is not available for the icon, raise an exception
             elif prefix and prefix_to_style.get(prefix) not in styles:
-                message = "prefix '{0}' is not found in {1}.\n Allowed prefix is {2} ".format(prefix, icon_name, styles)
+                raise FontAwesomeException(f"Prefix 'prefix' is not available for {icon_name}.\nThe icon is available in these styles: {styles}")
 
-                raise FontAwesomeException(message)
-
-            css_class = '{0} fa-{1}'.format(prefix, icon_name)
-            if size:
-                css_class += " " + size
-            el.attrib = {'class': css_class}
-            return el
-        message = "{0} isn't a FA icon I know about".format(icon_name)
-        raise FontAwesomeException(message)
+            # Everything is fine, set the class attribute
+            style = prefix_to_style.get(prefix)
+            el.set('class', f'fa-{style} fa-{icon_name} {size}' if size else f'fa-{style} fa-{icon_name}')
+            return el, m.start(0), m.end(0)
+        else:
+            raise FontAwesomeException(f"Unknown icon name: {icon_name}")
 
 
 class FontAwesomeExtension(Extension):
-    'Pick a good spot for calling the pattern defined above'
-
-    def extendMarkdown(self, md, md_globals):
-        fontawesome = FontAwesomePattern(fontawesome_pattern)
-        md.inlinePatterns.add('fontawesome', fontawesome, '<reference')
+    def extendMarkdown(self, md):
+        md.inlinePatterns.register(FontAwesomeInlineProcessor(fontawesome_pattern, md), 'fontawesome', 175)
 
 
-def makeExtension(*args, **kwargs):
-    return FontAwesomeExtension(*args, **kwargs)
+def makeExtension(**kwargs):
+    return FontAwesomeExtension(**kwargs)
