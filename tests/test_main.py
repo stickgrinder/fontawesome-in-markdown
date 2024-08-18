ad18e2ac2c073e8a8e85a14c7c098e5e2fd52f73
Paolo Pustorino <stickgrinder@gmail.com>
2024-08-18 15:10:54 +0200

    Test suite has been updated to FontAwesome 6.x.

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,17 +2,19 @@
 from __future__ import unicode_literals
 import pytest
 from markdown import Markdown
-from fontawesome_markdown import FontAwesomeExtension, FontAwesomeException
+from fontawesome_markdown import FontAwesomeInlineProcessor, FontAwesomeException, FontAwesomeExtension
 
-
-@pytest.fixture(params=[FontAwesomeExtension(), 'fontawesome_markdown'], ids=["import", "string"])
+@pytest.fixture(params=[
+    FontAwesomeExtension(),
+    'fontawesome_markdown'
+], ids=["import", "string"])
 def fa_markdown(request):
     return Markdown(extensions=[request.param])
 
 
 def test_example(fa_markdown):
-    expected_markup = '<p>i ♥ <i class="fa fa-coffee"></i></p>'
-    assert fa_markdown.convert('i ♥ :fa-coffee:') == expected_markup
+    expected_markup = '<p>i ♥ <i class="fa-solid fa-mug-hot"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-mug-hot:') == expected_markup
 
 
 def test_unknown_raises(fa_markdown):
@@ -23,48 +25,48 @@ def test_unknown_raises(fa_markdown):
 
 
 def test_prefix_not_found_raises(fa_markdown):
-    with pytest.raises(FontAwesomeException, match="prefix 'fa' is not found in facebook"):
+    with pytest.raises(FontAwesomeException, match="Prefix 'prefix' is not available for facebook."):
         fa_markdown.convert("i ♥ :fa fa-facebook:")
     
 
 def test_size(fa_markdown):
-    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-xs"></i></p>'
-    assert fa_markdown.convert('i ♥ :fa-coffee fa-xs:') == expected_markup
+    expected_markup = '<p>i ♥ <i class="fa-solid fa-mug-hot fa-xs"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-mug-hot fa-xs:') == expected_markup
 
-    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-sm"></i></p>'
-    assert fa_markdown.convert('i ♥ :fa-coffee fa-sm:') == expected_markup
+    expected_markup = '<p>i ♥ <i class="fa-solid fa-mug-hot fa-sm"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-mug-hot fa-sm:') == expected_markup
 
-    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-lg"></i></p>'
-    assert fa_markdown.convert('i ♥ :fa-coffee fa-lg:') == expected_markup
+    expected_markup = '<p>i ♥ <i class="fa-solid fa-mug-hot fa-lg"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-mug-hot fa-lg:') == expected_markup
 
-    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-1x"></i></p>'
-    assert fa_markdown.convert('i ♥ :fa-coffee fa-1x:') == expected_markup
+    expected_markup = '<p>i ♥ <i class="fa-solid fa-mug-hot fa-1x"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-mug-hot fa-1x:') == expected_markup
 
-    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-3x"></i></p>'
-    assert fa_markdown.convert('i ♥ :fa-coffee fa-3x:') == expected_markup
+    expected_markup = '<p>i ♥ <i class="fa-solid fa-mug-hot fa-3x"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-mug-hot fa-3x:') == expected_markup
 
-    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-10x"></i></p>'
-    assert fa_markdown.convert('i ♥ :fa-coffee fa-10x:') == expected_markup
+    expected_markup = '<p>i ♥ <i class="fa-solid fa-mug-hot fa-10x"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-mug-hot fa-10x:') == expected_markup
 
 
 def test_fab_icon(fa_markdown):
-    expected_markup = '<p>i ♥ <i class="fab fa-facebook"></i></p>'
+    expected_markup = '<p>i ♥ <i class="fa-brands fa-facebook"></i></p>'
     assert fa_markdown.convert('i ♥ :fa-facebook:') == expected_markup
 
 
 def test_fab_icon_with_prefix(fa_markdown):
-    expected_markup = '<p>i ♥ <i class="fab fa-facebook"></i></p>'
+    expected_markup = '<p>i ♥ <i class="fa-brands fa-facebook"></i></p>'
     assert fa_markdown.convert('i ♥ :fab fa-facebook:') == expected_markup
 
 
 def test_far_icon_with_prefix(fa_markdown):
-    expected_markup = '<p>i ♥ <i class="far fa-font-awesome-logo-full"></i></p>'
-    assert fa_markdown.convert('i ♥ :far fa-font-awesome-logo-full:') == expected_markup
+    expected_markup = '<p>i ♥ <i class="fa-regular fa-star"></i></p>'
+    assert fa_markdown.convert('i ♥ :far fa-star:') == expected_markup
 
 
 def test_fa_icon_without_prefix(fa_markdown):
-    expected_markup = '<p>i ♥ <i class="fa fa-font-awesome-logo-full"></i></p>'
-    assert fa_markdown.convert('i ♥ :fa-font-awesome-logo-full:') == expected_markup
+    expected_markup = '<p>i ♥ <i class="fa-solid fa-star"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-star:') == expected_markup
 
 
 # light style not found in icons.json...
